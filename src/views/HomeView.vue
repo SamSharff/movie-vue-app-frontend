0b805d209ea1284d78d48eccad156ba77ccd5dd8
Sam Sharff <samanthaleec@gmail.com>
2022-04-12 16:21:42 -0400

    fixes movies.index; tests successfully

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Movies! Movies! Movies!",
+      movies: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("http://localhost:3000/movies.json").then((response) => {
+        this.movies = response.data;
+        console.log("All movies", this.movies);
+      });
+    },
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h2>Title: {{ movie.title }}</h2>
+      <p>Year: {{ movie.year }}</p>
+      <p>Plot: {{ movie.plot }}</p>
+      -->
+    </div>
   </div>
 </template>
 
