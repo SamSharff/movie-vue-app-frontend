7d7e8ecd9ddab87e500933f8a771db9c0681e402
Sam Sharff <samanthaleec@gmail.com>
2022-04-12 18:04:13 -0400

    adds show action with questions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,8 @@ export default {
     return {
       message: "Movies! Movies! Movies!",
       movies: [],
+      newMovieParams: {},
+      currentMovie: {},
     };
   },
   created: function () {
@@ -18,6 +20,19 @@ export default {
         console.log("All movies", this.movies);
       });
     },
+    createMovie: function () {
+      console.log("Creating your movie!");
+
+      axios.post("http://localhost:3000/movies.json", this.newMovieParams).then((response) => {
+        console.log("Sucess!", response.data);
+        this.movies.push(response.data);
+      });
+    },
+    showMovie: function (movie) {
+      console.log(movie);
+      this.currentMovie = movie;
+      document.querySelector("#movie-details").showModal();
+    },
   },
 };
 </script>
@@ -25,11 +40,60 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <div v-for="movie in movies" v-bind:key="movie.id">
-      <h2>Title: {{ movie.title }}</h2>
-      <p>Year: {{ movie.year }}</p>
-      <p>Plot: {{ movie.plot }}</p>
-      -->
+    <h2>New movie!</h2>
+    <div>
+      Title:
+      <input type="text" v-model="newMovieParams.title" />
+      <br />
+      Year:
+      <input type="text" v-model="newMovieParams.year" />
+      <br />
+      Plot:
+      <input type="text" v-model="newMovieParams.plot" />
+      <br />
+      Director:
+      <input type="text" v-model="newMovieParams.director" />
+      <br />
+      <!-- English (True/False):
+      <input type="text" v-model="newMovieParams.english" /> -->
+    </div>
+
+    <br />
+
+    <button v-on:click="createMovie">Add a movie</button>
+    <div>
+      <div v-for="movie in movies" v-bind:key="movie.id">
+        <h2>Title: {{ movie.title }}</h2>
+        <p>Year: {{ movie.year }}</p>
+        <p>Plot: {{ movie.plot }}</p>
+        <button v-on:click="showMovie(movie)">More info</button>
+      </div>
+
+      <dialog id="movie-details">
+        <form method="dialog">
+          <h1>Movie info</h1>
+          <p>
+            Title:
+            <input type="text" v-model="currentMovie.title" />
+          </p>
+          <p>
+            Year:
+            <input type="text" v-model="currentMovie.year" />
+          </p>
+          <p>
+            Plot:
+            <input type="text" v-model="currentMovie.plot" />
+          </p>
+          <p>
+            Director:
+            <input type="text" v-model="currentMovie.director" />
+          </p>
+          <!-- <p>
+            English:
+            <input type="text" v-model="currentMovie.english" /> -->
+          <!-- </p> -->
+        </form>
+      </dialog>
     </div>
   </div>
 </template>
