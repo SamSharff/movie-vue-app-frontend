532d4034151619bf45bc97b20868ce58e5b34c9b
Sam Sharff <samanthaleec@gmail.com>
2022-04-14 08:55:42 -0400

    adds MoviesIndex to src/views; adds route in index.js; adds link in App.vue; successfully tests in browser

---
+++ src/views/MoviesIndex.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      movies: [],
+    };
+  },
+  created: function () {
+    axios.get("http://localhost:3000/movies.json").then((response) => {
+      this.movies = response.data;
+      console.log("All movies", this.movies);
+    });
+  },
+};
+</script>
+
+<template>
+  <div>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h2>Title: {{ movie.title }}</h2>
+      <p>Year: {{ movie.year }}</p>
+      <p>Plot: {{ movie.plot }}</p>
+      <button v-on:click="showMovie(movie)">More info</button>
+    </div>
+  </div>
+</template>
