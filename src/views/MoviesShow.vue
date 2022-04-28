9a6d5c166238ecee4e4b2c082d65a751c49b3460
Sam Sharff <samanthaleec@gmail.com>
2022-04-28 11:01:17 -0400

    adds destroy action method and destroyMovie button to MoviesShow.vue

--- src/views/MoviesShow.vue
+++ src/views/MoviesShow.vue
@@ -13,6 +13,14 @@ export default {
       this.movie = response.data;
     });
   },
+  methods: {
+    destroyMovie: function (movie) {
+      axios.delete("/movies/" + movie.id + ".json").then((response) => {
+        console.log("Destroying movie", response);
+        this.$router.push("/movie");
+      });
+    },
+  },
 };
 </script>
 <template>
@@ -22,8 +30,11 @@ export default {
     <p>Year: {{ movie.year }}</p>
     <p>Plot: {{ movie.plot }}</p>
     <p>Director: {{ movie.director }}</p>
+
+    <router-link v-bind:to="`/movies/${movie.id}/edit`">Edit movie</router-link>
+    <br />
     <router-link to="/movies">Back to database</router-link>
     <br />
-    <router-link v-bind:to="`/movies/${movie.id}/edit`">Edit movie</router-link>
+    <button v-on:click="destroyMovie(movie)">Delete movie</button>
   </div>
 </template>
